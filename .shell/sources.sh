97d81939443422899a950b2610e1a43a8f0fbe94
Michael Ketiku <mketiku@gmail.com>
2019-11-10 22:26:46 -0500

    Update source.sh to lazy load virtualenvwrapper

--- .shell/sources.sh
+++ .shell/sources.sh
@@ -1,3 +1,3 @@
-[ -f /usr/local/bin/virtualenvwrapper.sh ] &&  source /usr/local/bin/virtualenvwrapper.sh
+[ -f /usr/local/bin/virtualenvwrapper_lazy.sh ] &&  source /usr/local/bin/virtualenvwrapper_lazy.sh
 
 [ -f ~/.fzf.zsh ] && source $HOME/.fzf.zsh
